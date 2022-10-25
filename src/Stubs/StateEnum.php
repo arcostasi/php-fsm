07cd8f73869a74358d8f3063e21e44c8cf07e287
jagarsoft <jagarsoft2000@gmail.com>
2022-10-25 23:27:17 +0200

    This version tries to take advantage of the new features introduced into v2.x

--- src/Stubs/StateEnum.php
+++ src/Stubs/StateEnum.php
@@ -3,7 +3,7 @@
 namespace Arcostasi\PhpStateMachine\Stubs;
 
 class StateEnum {
-    const STATE_1 = 'GREEN';
-    const STATE_2 = 'YELLOW';
-    const STATE_3 = 'RED';
+    const GREEN = 'GREEN';
+    const YELLOW = 'YELLOW';
+    const RED = 'RED';
 }
